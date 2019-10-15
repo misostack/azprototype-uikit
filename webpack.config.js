b71e51fb55b32ad60d9a0e3fad4828b31d1f2641
misostack <misostack.com@gmail.com>
2019-10-15 11:20:53 +0700

    Init azprototype uikit

---
+++ webpack.config.js
@@ -0,0 +1,111 @@
+const path = require('path');
+const src = path.resolve(__dirname, 'src');
+const dist = path.resolve(__dirname, 'docs');
+const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const getRules = function (mode) {
+    const rules = [
+        // js, jsx
+        {
+            test: /\.(js|jsx)$/,
+            exclude: /node_module/,
+            use: ['babel-loader']
+        },
+        // css,scss
+        {
+            test: /\.(css|scss)$/,
+            use: [
+                // fallback to style-loader in development
+                mode !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+                "css-loader",
+                "sass-loader"
+            ]
+        },
+        // images
+        {
+            test: /\.(png|svg|jpe?g|gif)$/i,
+            use:[
+                {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[path][name].[ext]?[hash]',
+                        context: 'src',
+                        outputPath: '',
+                        useRelativePath: false
+                    }
+                }
+            ]
+        },
+    ];
+    return rules;
+}
+
+const getPlugins = function (mode) {
+    let plugins = [
+        new webpack.ProgressPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(src, 'index.html'),
+            filename: 'index.html',
+            // inject: 'body'
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "assets/css/[name]_[hash].css",
+            chunkFilename: "assets/css/[id]_[hash].css"
+        })
+    ];
+    if (mode === 'production') {
+        plugins.push(new CleanWebpackPlugin());
+    }
+    return plugins;
+}
+
+let config = {
+    entry: {
+        index: path.resolve(src, 'index.js')
+    },
+    output: {
+        filename: '[name].js',
+        path: dist,
+        publicPath: ''
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        port: 8081,
+        contentBase: dist,
+        hot: true
+    },
+    module: {
+        rules: []
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    plugins: []
+};
+
+module.exports = (env, argv) => {
+    if (argv.mode === 'development') {
+        config.devtool = 'source-map';
+        config.output = {
+            filename: '[name].js',
+            path: dist
+        };
+    }
+
+    if (argv.mode === 'production') {
+        config.output = {
+            filename: '[name]_[hash].min.js',
+            path: dist
+        };
+    }
+
+    config.module.rules = getRules(argv.mode);
+    config.plugins = getPlugins(argv.mode);
+
+    return config;
+}
